495891c376e0a6bb4f2575366dc0c2c125216d1a
Rajshree <rajshreejavadeveloper@gmail.com>
2024-06-12 15:17:30 +0530

    udpate

---
+++ src/main/java/org/example/SystemDesign/lowLevelDesign/designPatterns/behavioural/mediatorDesignPattern/Main.java
@@ -0,0 +1,14 @@
+package org.example.SystemDesign.lowLevelDesign.designPatterns.behavioural.mediatorDesignPattern;
+
+/*
+* Mediator Design Pattern
+* LLD: Online Auction System
+* - The Mediator Pattern is a behavioural design pattern.
+* - It encourage loose coupling by keeping objects from referring to each other explicitly
+*  and allows them to communicate through a mediator object.
+*
+*
+*
+* */
+public class Main {
+}
