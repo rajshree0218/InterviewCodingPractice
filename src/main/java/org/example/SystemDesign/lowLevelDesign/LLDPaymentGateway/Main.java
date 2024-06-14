76d05d779e9ae3ce68c5e779061ea7a3e40783b1
Rajshree <rajshreejavadeveloper@gmail.com>
2024-06-14 07:26:48 +0530

    design payment gateway

---
+++ src/main/java/org/example/SystemDesign/lowLevelDesign/LLDPaymentGateway/Main.java
@@ -0,0 +1,20 @@
+package org.example.SystemDesign.lowLevelDesign.LLDPaymentGateway;
+
+public class Main {
+
+    /*
+    * What is Payment Gateway
+    * In short, Payment Gateway acts as a mediator because USER and Financial Institution,
+    * and helps transfer money.
+    *
+    *           Payment Gateway
+    *           /           \
+    *          /             \
+    *         /               \
+    * Peer to Peer           Peer to Merchant
+    *
+    *
+    * 1. Clarify the requirements / Understand the flow.
+    *
+    * */
+}
